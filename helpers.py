ba40239a3e0620f0c8d6f515d8b021cf5a54b5af
iamkris18 <krishnas.ee19@rvce.edu.in>
2024-12-17 19:16:48 +0530

    logging errors

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import psycopg2
 
 def load_invoice_data():
@@ -7,14 +8,21 @@ def load_invoice_data():
             data = json.load(file)
         return data
     except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
         print("Error loading data.json:", e)
         return []
 
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="palcode_db",
-        user="postgres",
-        password="password"
-    )
-    return conn
+    try: 
+        conn = psycopg2.connect(
+            host="localhost",
+            database="palcode_db",
+            user="postgres",
+            password="password"
+        )
+        return conn
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}")
+        print("Error loading data.json:", e)
+        return []
+
